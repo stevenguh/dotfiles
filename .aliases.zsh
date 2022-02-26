7cfe08337d9c02ede49994d092c11213b5c8d2c5
Steven Guh <3455662+stevenguh@users.noreply.github.com>
2022-02-25 23:28:12 -0800

    Add `-F` to `ls` alias to append indicator

--- .aliases.zsh
+++ .aliases.zsh
@@ -2,7 +2,7 @@ alias shutdown='sudo shutdown now'
 alias restart='sudo reboot'
 alias suspend='sudo pm-suspend'
 
-alias ls="${aliases[ls]:-ls} -A -h"
+alias ls="${aliases[ls]:-ls} -A -h -F"
 alias ll="ls -l"
 
 alias cp='cp -iv'
